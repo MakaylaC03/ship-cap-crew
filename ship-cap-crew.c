248aa1e61eaa4ba6592fa1a674f45279bad97bf3
Makayla Coomer <makayla.coomer@outlook.com>
2024-02-14 16:31:15 -0600

    Added Multiplayer Funtionality

--- ship-cap-crew.c
+++ ship-cap-crew.c
@@ -4,80 +4,54 @@
 
 void roller(int roll[5], int size, int start);
 int checker(int roll[5], int check, int start);
+int validplay(int roll[5]);
+void handreset(int roll[5]);
+int playercount();
 
 int main(){
     srand(time(NULL));                  //initialization
     int roll[5];
     int i =0;
-    int ship=0;
-    int capt =0;
-    int crew =0;
-    int rollc=0;
+    int players = playercount();
+    int score[players];
+   
 
-    roller(roll, 5, 0);                 // first roll
-    
-
-    while(rollc<3){
-    rollc++;                                //increment roll
-    ship=checker(roll, 6, 0);               //checking rolls for 6
-    if (ship==1){
-        printf("You have a ship! \n");
-        capt=checker(roll, 5, 1);           // checking roll for 5
-        if (capt==1){
-            printf("You have a captain! \n");
-            crew=checker(roll,4,2);             //check roll for 4
-            if(crew==1){
-                printf("You have a crew! \n");  //leave loop if all are present
-                break;
-            }
-            else{
-
-                if(rollc==3){                       //Leave if on roll 3 or roll again
-                    break;
-                }
-                roller(roll,5,2);
-                
-                crew=checker(roll,4,2);             //Check again
-                if(crew==1){
-                 printf("You have a crew! \n");
-                 rollc++;
-                break;
-                }
-            
-            }
-        }
-        else{
-            if(rollc==3){                           //leaves if on roll 3
-                break;
-            }
-            roller(roll,5,1);
-            
-        }
-    } 
-    else{                                          //Leaves if on roll 3
-        if(rollc==3){
-            break;
-        }
-        roller(roll, 5, 0);
-       
-    } 
-    }
+    for( int j = 0; j<players;j++){
+    printf("Starting player %d\n", j+1);
+    int play = 0;
+    play = validplay(roll);
 
     printf("Your Final Hand: ");
     for(i=0;i<5; i++){
         printf("%d", roll[i]);
     }
 
-    int p1points = roll[3]+roll[4];
-    if(crew==1){                                                                       //Will need to be changed, to add on more players
-        printf("\nYou have %d points, you have %d rolls left \n", p1points, 3-rollc);
-        if(rollc<3){
-            printf("Would you like to roll or hold?");
-        }
+    int points = roll[3]+roll[4];
+    if(play==1){                                                                       //Will need to be changed, to add on more players
+        printf("\nYou have %d points, \n", points);
+        score[j] = points;
+    }
+    else{
+        printf("\nPlay invalid: Score 0\n");
+        score[j]=0;
     }
 
+    handreset(roll);
 
+    } 
+
+    for(i=0;i<players; i++){
+        printf("Player %d score: %d\n", i+1,score[i]);
+    }
 return 0;
+}
+
+int playercount(){
+    int playeramt;
+    printf("How many players: ");
+    scanf("%d", &playeramt);
+    return playeramt;
+
 }
 
 
@@ -129,6 +103,78 @@ int checker(int roll[5], int check, int start){
     }
     else{
         return 0;
+    }
+
+}
+
+int validplay(int roll[5]){
+     int ship=0;
+    int capt =0;
+    int crew =0;
+    int rollc=0;
+
+   
+    rollc=0;
+    roller(roll, 5, 0);                 // first roll
+    
+
+    while(rollc<3){
+    rollc++;                                //increment roll
+    ship=checker(roll, 6, 0);               //checking rolls for 6
+    if (ship==1){
+        printf("You have a ship! \n");
+        capt=checker(roll, 5, 1);           // checking roll for 5
+        if (capt==1){
+            printf("You have a captain! \n");
+            crew=checker(roll,4,2);             //check roll for 4
+            if(crew==1){
+                printf("You have a crew! \n");  //leave loop if all are present
+                break;
+            }
+            else{
+
+                if(rollc==3){                       //Leave if on roll 3 or roll again
+                    break;
+                }
+                roller(roll,5,2);
+                
+                crew=checker(roll,4,2);             //Check again
+                if(crew==1){
+                 printf("You have a crew! \n");
+                 rollc++;
+                break;
+                }
+            
+            }
+        }
+        else{
+            if(rollc==3){                           //leaves if on roll 3
+                break;
+            }
+            roller(roll,5,1);
+            
+        }
+    } 
+    else{                                          //Leaves if on roll 3
+        if(rollc==3){
+            break;
+        }
+        roller(roll, 5, 0);
+       
+    } 
+    }
+
+    if(crew==1){
+        return 1;
+    }
+    return 0;
+
+}
+
+void handreset(int roll[5]){
+    int i;
+    for(i=0;i<5;i++){
+        roll[i]=0;
     }
 
 }
